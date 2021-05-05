f81fd6c21b09eeddd0e1491b24b857f2c1f39f00
Thiago Bezerra <thiago.bezerra@digitallworkers.com>
2021-05-05 17:08:59 -0300

    Add bootstrap and css styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="text-primary">
       <h1>Olá Mundo !</h1>
     </div>
   );
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,8 @@ import './index.css';
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'assets/css/styles.css';
 
 ReactDOM.render(
   <React.StrictMode>

9cac51c77fabbcad5b22307ef9d70ba0d8053d41
Thiago Bezerra <thiago.bezerra@digitallworkers.com>
2021-05-09 04:16:22 -0300

    Change footer

--- frontend/src/components/footer/footer.tsx
+++ frontend/src/components/footer/footer.tsx
@@ -13,18 +13,7 @@ function Footer() {
           </a>
         </p>
         <p className="text-light">
-          <small>
-            <strong>Semana Spring React</strong>
-            <br />
-            Evento promovido pela escola DevSuperior:{" "}
-            <a
-              href="https://instagram.com/devsuperior.ig"
-              target="_blank"
-              rel="noreferrer"
-            >
-              @devsuperior.ig
-            </a>
-          </small>
+          <strong>Spring + React</strong>
         </p>
       </div>
     </footer>

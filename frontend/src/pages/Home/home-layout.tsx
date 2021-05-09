50eed66a1622a9b2bfd6d55c7d9eed71c30c9a18
Thiago Bezerra <thiago.bezerra@digitallworkers.com>
2021-05-08 21:56:48 -0300

    Add navegation components

--- frontend/src/pages/Home/home-layout.tsx
+++ frontend/src/pages/Home/home-layout.tsx
@@ -1,5 +1,31 @@
+import NavBar from "components/navbar/navbar";
+import Footer from "components/footer/footer";
+import "./home-layout.css";
+import { Link } from "react-router-dom";
+
 function HomeLayout() {
-  return <div className=""></div>;
+  return (
+    <>
+      <NavBar />
+      <div className="container">
+        <div className="jumbotron config-jumbotron">
+          <h1 className="display-4">DSVendas</h1>
+          <p className="lead">
+            Analise o desempenho das suas vendas por diferentes perspectivas
+          </p>
+          <hr />
+          <p>
+            Esta aplicação consiste em exibir um dashboard a partir de dados
+            fornecidos por um back end construído com Spring Boot.
+          </p>
+          <Link className="btn btn-primary btn-lg" to="/dashboard">
+            Acessar Dashboard
+          </Link>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default HomeLayout;
